5d050c635c682eaba2a86e0d6a09b21742109c44
John Choi <jchoi950@Johns-MacBook-Pro.local>
2020-01-22 12:12:08 -0800

    shuffle cards at the start of game

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,8 +34,51 @@ function unhideCard () {
     secondCardClicked.classList.remove("hidden");
     clearCardClicked();
     addClicker();
+    console.log("Timer ranout");
     },1500);
 };
+
+function shuffleCards () {
+    var cardCount = document.getElementById("gameCards").childElementCount
+    var cardImgClass = $("div.card-front").attr("class").split(" ");
+    var frontCards = document.querySelectorAll(".card-front");
+
+    for (i = 0; i < cardCount; i++) {
+        frontCards[i].className = "";
+        frontCards[i].className += "card-front";
+    };
+
+    var classImgArray = [
+        "js-logo",
+        "js-logo",
+        "html-logo",
+        "html-logo",
+        "gitHub-logo",
+        "gitHub-logo",
+        "mysql-logo",
+        "mysql-logo",
+        "php-logo",
+        "php-logo",
+        "react-logo",
+        "react-logo",
+        "node-logo",
+        "node-logo",
+        "css-logo",
+        "css-logo",
+        "docker-logo",
+        "docker-logo"
+    ];
+
+    for (i = classImgArray.length - 1; i >= 0; i--) {
+        var randomIndex = Math.floor(Math.random() * i);
+        var tempIndex = classImgArray[i];
+
+        classImgArray[i] = classImgArray[randomIndex];
+        classImgArray[randomIndex] = tempIndex;
+
+        frontCards[i].classList.add(classImgArray[i]);
+    };
+}
 
 function handleClick (event) {
     if (event.target.className.indexOf("card-back") === -1) {
@@ -47,17 +90,20 @@ function handleClick (event) {
         secondCardClicked = event.target;
         secondCardClass = secondCardClicked.previousElementSibling.className;
         removeClicker();
-
+        console.log("second card")
         if (firstCardClass === secondCardClass) {
             addClicker();
             clearCardClicked();
             matches++
+            console.log("img matches")
         } else {
             unhideCard();
+            console.log("img does not match")
         }        
     } else {
        firstCardClicked = event.target;
        firstCardClass = firstCardClicked.previousElementSibling.className;
+       console.log("first card")
     }
 
     if (matches === maxMatches) {
@@ -66,3 +112,4 @@ function handleClick (event) {
 }
 
 addClicker();
+shuffleCards();
